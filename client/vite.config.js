3de5d8371090048eb0b45f54561b410e0bde4955
Maxime <maxime.huynh@okolo.fr>
2025-02-26 19:55:49 +0100

    add server url

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,31 +1,34 @@
-import { defineConfig } from 'vite';
+import { defineConfig,loadEnv  } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  build: {
-    outDir: 'build',
-    sourcemap: process.env.GENERATE_SOURCEMAP === 'true',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: ['./src/index', './index.html']
-    },
-  },
-  plugins: [react()],
-  server: {
-    open: true,
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
+export default defineConfig(({ command, mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    build: {
+      outDir: 'build',
+      sourcemap: process.env.GENERATE_SOURCEMAP === 'true',
+      emptyOutDir: true,
+      rollupOptions: {
+        input: ['./src/index', './index.html']
       },
     },
-  },
-  resolve: {
-    alias: {
-      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
-      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    plugins: [react()],
+    server: {
+      open: true,
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_SERVER_URL,
+          changeOrigin: true,
+        },
+      },
     },
-  },
-}
-);
+    resolve: {
+      alias: {
+        // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+        '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+      },
+    },
+  }
+});
--- client/vite.env.d.ts
+++ client/vite.env.d.ts
@@ -6,8 +6,12 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_MESSAGINGSENDERID: string;
   readonly VITE_FIREBASE_APPID: string;
   readonly VITE_FIREBASE_MESUREMENTID: string;
-  readonly VITE_EBAY_SEARCHITEMS_URL: string;
   readonly VITE_FIREBASE_FUNCTION_UPDATE_EBAYPRICES_FOR_CARDID: string;
+
+  readonly VITE_EBAY_SEARCHITEMS_URL: string;
+
+  readonly VITE_SERVER_URL:string;
+
 }
 
 interface ImportMeta {
